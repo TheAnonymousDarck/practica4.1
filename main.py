5dba0fc6b45f52fc3452d5612e06d010b785d618
Enrique <k.ventus250@gmail.com>
2022-09-25 21:46:51 -0500

    REFACTOR: main updated add HTMLTestRunner

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
+import HtmlTestRunner
+import unittest
+import time
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from decouple import config
-import time
 
-site = config('URL_SCRAPPING')
-driver = webdriver.Chrome()
-action = ActionChains(driver)
 
-driver.maximize_window()
-driver.get(site)
-time.sleep(2)
+class TestDragAndDrop(unittest.TestCase):
+    def setUp(self):
+        self.site = config('URL_SCRAPPING')
+        self.driver = webdriver.Chrome()
+        self.action = ActionChains(self.driver)
+        self.answers = {
+            '//*[@id="box1"]': '//*[@id="box101"]',
+            '//*[@id="box2"]': '//*[@id="box102"]',
+            '//*[@id="box3"]': '//*[@id="box103"]',
+            '//*[@id="box4"]': '//*[@id="box104"]',
+            '//*[@id="box5"]': '//*[@id="box105"]',
+            '//*[@id="box6"]': '//*[@id="box106"]',
+            '//*[@id="box7"]': '//*[@id="box107"]',
+        }
+        # time.sleep(2)
 
-answers = {
-    '//*[@id="box1"]': '//*[@id="box101"]',
-    '//*[@id="box2"]': '//*[@id="box102"]',
-    '//*[@id="box3"]': '//*[@id="box103"]',
-    '//*[@id="box4"]': '//*[@id="box104"]',
-    '//*[@id="box5"]': '//*[@id="box105"]',
-    '//*[@id="box6"]': '//*[@id="box106"]',
-    '//*[@id="box7"]': '//*[@id="box107"]',
-}
+    def test_drag_and_drop(self):
+        self.driver.get(self.site)
+        self.driver.maximize_window()
 
-for x, y in answers.items():
-    src = driver.find_element(By.XPATH, x)
-    dest = driver.find_element(By.XPATH, y)
-    action.drag_and_drop(src, dest).perform()
-    time.sleep(1)
+        for key, value in self.answers.items():
+            element = self.driver.find_element(By.XPATH, key)
+            target = self.driver.find_element(By.XPATH, value)
+            self.action.drag_and_drop(element, target).perform()
+            time.sleep(1)
 
-driver.quit()
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='example_dir'))
